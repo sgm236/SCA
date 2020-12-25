e1e20fed88c226c89d930547ab24e9f95ec557a7
sil <sil@gmail.com>
2020-12-25 19:22:13 +0900

    구구단

---
+++ 10314/src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=2; i<=9; i++) {
+			for(int j=1; j<=9; j++) {
+				System.out.print(i+"*"+j+"="+(i*j)+" ");
+			}
+			System.out.println("");
+		}
+	}
+
+}
